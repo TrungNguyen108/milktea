978b7e075bbad7f462d20cc3c8e4270f280b9cca
Trung Nguyen <58510695+TrungNguyen108@users.noreply.github.com>
2023-03-06 09:17:08 +0700

    update

--- lib/common/api_request.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:dio/dio.dart';
-// import 'dio_client.dart';
-//
-// class ApiRequest {
-//   late Dio _dio;
-//   ApiRequest(){
-//     _dio = DioClient.instance.dio;
-//   }
-// }
