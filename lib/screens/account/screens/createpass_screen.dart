414a1586b8a67af9969ee3a2804b4329844f373b
Trung Nguyen <58510695+TrungNguyen108@users.noreply.github.com>
2023-03-08 15:31:51 +0700

    update

--- lib/screens/account/screens/createpass_screen.dart
+++ lib/screens/account/screens/createpass_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:material_dialogs/dialogs.dart';
+import 'package:material_dialogs/widgets/buttons/icon_button.dart';
 
 import '../../product/screens/home_screens.dart';
+
 
 
 class Createpass_screen extends StatefulWidget {
@@ -11,8 +14,11 @@ class Createpass_screen extends StatefulWidget {
 }
 
 class _Createpass_screenState extends State<Createpass_screen> {
-  bool valuefirst = false;
   bool isChecked = false;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _password = TextEditingController();
+  TextEditingController _repassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,84 +42,125 @@ class _Createpass_screenState extends State<Createpass_screen> {
                 child: Image.asset('assets/images/taomk.png',),
               ),
               SizedBox(
-                child:Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                      child: Align(child: Text('Nhập lại mật khẩu mới của bạn ',style: TextStyle(
-                        fontSize: 15,
-                        color: Color(0xFF9A9A9A),
-                        fontWeight: FontWeight.w300,
-                      ),),alignment: Alignment.centerLeft,),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                      child: const TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          fillColor: Color(0xFFF5F5FA),
-                          filled: true,
-                          border: InputBorder.none,
-                          labelText: 'Mật khẩu ',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                      child: const TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          fillColor: Color(0xFFF5F5FA),
-                          filled: true,
-                          border: InputBorder.none,
-                          labelText: 'Nhập lại mật khẩu',
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 40,
-                            height: 40,
-                            child: GFCheckbox(
-                              size: GFSize.LARGE,
-                              activeBgColor: Color(0xFFFFB9116),
-                              onChanged: (value) {
-                                setState(() {
-                                  isChecked = value;
-                                });
-                              },
-                              value: isChecked,
-                              inactiveIcon: null,
-                            ),
-                          ),
-                          Text("Nhớ mật khẩu",style: TextStyle(
-                              fontSize: 13
-                          ),)
-
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
-                      child: GFButton(
-                        onPressed: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => ProductScreen()));
-                        },
-                        text: "Đăng nhập",
-                        textStyle: TextStyle(
+                child:Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                        child: Align(child: Text('Nhập lại mật khẩu mới của bạn ',style: TextStyle(
                           fontSize: 15,
-                          fontWeight:FontWeight.w400,
-                        ),
-                        fullWidthButton: true,
-                        color: Color(0xFFFFB9116),
-                        shape: GFButtonShape.pills,
-                        size: GFSize.LARGE,
+                          color: Color(0xFF9A9A9A),
+                          fontWeight: FontWeight.w300,
+                        ),),alignment: Alignment.centerLeft,),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                        child: TextFormField(
+                          obscureText: true,
+                          controller: _password,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xFFF5F5FA),
+                            filled: true,
+                            border: InputBorder.none,
+                            labelText: 'Mật khẩu ',
+                          ),
+                          validator: (value){
+                            if (value == null || value.isEmpty) {
+                              return 'Vui lòng nhập mật khẩu';
+                            }else if(value.length < 6){
+                              return 'Mật khẩu phải trên 6 ký tự';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                        child: TextFormField(
+                          obscureText: true,
+                          controller: _repassword,
+                          decoration: InputDecoration(
+                            fillColor: Color(0xFFF5F5FA),
+                            filled: true,
+                            border: InputBorder.none,
+                            labelText: 'Nhập lại mật khẩu',
+                          ),
+                          validator: (value){
+                            if (value == null || value.isEmpty) {
+                              return 'Vui lòng nhập lại mật khẩu';
+                            }else if(value.length < 6){
+                              return 'Mật khẩu phải trên 6 ký tự';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 40,
+                              height: 40,
+                              child: GFCheckbox(
+                                size: GFSize.LARGE,
+                                activeBgColor: Color(0xFFFFB9116),
+                                onChanged: (value) {
+                                  setState(() {
+                                    isChecked = value;
+                                  });
+                                },
+                                value: isChecked,
+                                inactiveIcon: null,
+                              ),
+                            ),
+                            Text("Nhớ mật khẩu",style: TextStyle(
+                                fontSize: 13
+                            ),)
+
+                          ],
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+                        child: ElevatedButton(
+                          child: Text("Đăng nhập", style: TextStyle(fontSize: 15,fontWeight:FontWeight.w400,color: Color(0xFFFFFFFF),)),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFFFFB9116),
+                            padding: EdgeInsets.only(left: 40,right: 40)
+                          ),
+                          onPressed: (){
+                            if (_formKey.currentState!.validate()) {
+                              if(_password.text == _repassword.text){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductScreen()));
+                              }else{
+                                Dialogs.materialDialog(
+                                    context: context,
+                                    color: Colors.white,
+                                    title: 'Mật khẩu không trùng khớp, vui lòng nhập lại !',
+                                    actions: [
+                                      IconsButton(
+                                        onPressed: (){
+                                          Navigator.of(context).pop();
+                                        },
+                                        text: 'Ok',
+                                        iconData: Icons.check,
+                                        color: Color(0xFFFB9116),
+                                        textStyle: const TextStyle(color: Colors.white),
+                                        iconColor: Colors.white,
+                                      )
+                                    ]
+                                );
+                              }
+                            }
+                            // Navigator.push(context, MaterialPageRoute(builder: (context) => ProductScreen()));
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
